4b55c65d7b383ff1e3d07a3eafcf5a1a566ab94c
Israel Garcia <israel.z1@hotmail.com>
2021-02-05 00:55:14 -0300

    initial commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const router = express.Router()
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controlers/userControler.js'
+import { protect } from '../middleWare/authMiddleware.js'
+
+router.route('/').post(registerUser)
+router.post('/login', authUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
+
+export default router
---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
+import { saveShippingAddress } from '../actions/cartActions'
+
+const ShippingScreen = ({ history }) => {
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddres } = cart
+
+  const [address, setAddress] = useState(shippingAddres.address)
+  const [city, setCity] = useState(shippingAddres.city)
+  const [postalCode, setPostalCode] = useState(shippingAddres.postalCode)
+  const [country, setCountry] = useState(shippingAddres.country)
+
+  const dispatch = useDispatch()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    history.push('/payment')
+  }
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 />
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='address'>
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter address'
+            required
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='city'>
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter city'
+            required
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='postalCode'>
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter postal code'
+            required
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='country'>
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter country'
+            required
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default ShippingScreen
